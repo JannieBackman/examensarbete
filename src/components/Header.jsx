11946d79d98b9c1d313cd46acde6d74390333d1e
Jannie87 <jannie.backman@gmail.com>
2022-12-20 14:19:06 +0100

    created a navbar for the header and inserted image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import brandName from './assets/text_logo.png';
+import './Header.css';
+
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header-container">
+      <nav className="navbar">
+        <img src={brandName} alt="brandName" />
+        <div className="nav-menu">
+          <ul>
+            <a href="Services">
+              <li>Tjänster</li>
+            </a>
+            <a href="About">
+              <li>Om mig</li>
+            </a>
+            <a href="contact">
+              <li>Kontakt</li>
+            </a>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
