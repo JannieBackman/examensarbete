2f6f824200ae71acd9393e3a39ddd335e18bc4d1
Jannie87 <jannie.backman@gmail.com>
2022-12-21 13:37:21 +0100

    added text avbout products

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-
-import Header from "./Header";
-import Hero from "./Hero";
-import Text from "./Text";
+import Header from './Header';
+import Hero from './Hero';
+import Text from './Text';
 import Footer from './Footer';
-
-
+import Products from './Products';
 
 function App() {
   return (
     <div>
       <Hero />
       <Text />
+      <Products />
       <Footer />
     </div>
   );
