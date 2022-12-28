794ed0ce5f80682c4071421d5c5663ee422f02bb
elinarnten <elin.arnten@hotmail.com>
2022-12-28 12:38:12 +0100

    centered the text and made the container higher

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,18 +1,20 @@
-import Typewriter from 'typewriter-effect';
-import './Text.css';
+import Typewriter from "typewriter-effect";
+import "./Text.css";
 
 function Text() {
   return (
-    <div className="textblock">
-      <Typewriter
-        options={{
-          strings: [
-            'Rörelseglädje - Träna tillsammans - Må bra - Funktionell träning ',
-          ],
-          autoStart: true,
-          loop: true,
-        }}
-      />
+    <div className='textblock'>
+      <span className='writer'>
+        <Typewriter
+          options={{
+            strings: [
+              "Rörelseglädje - Träna tillsammans - Må bra - Funktionell träning ",
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </span>
     </div>
   );
 }
