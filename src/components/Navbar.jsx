c6d7260ab2f3812f6f1f59882f2a49781616063c
Jannie87 <jannie.backman@gmail.com>
2023-01-03 09:19:38 +0100

    Added a offcanvas Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-function Navbar() {
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import brandName from './assets/text_logo.png';
+import React from 'react';
+
+function NavMobile() {
+  const [show, setShow] = React.useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        onClick={handleShow}
+      >
+        <span class="material-symbols-outlined">menu</span>
+        <img src={brandName} alt="brandName" />
+      </div>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ul>
+            <a href="#product-section">
+              <li>Tjänster</li>
+            </a>
+            <a href="#about-section">
+              <li>Om mig</li>
+            </a>
+            <a href="#contact-section">
+              <li>Kontakt</li>
+            </a>
+          </ul>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
-export default Navbar;
+export default NavMobile;
