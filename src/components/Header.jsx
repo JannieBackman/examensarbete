499ba7699fffce7b9f11f2e7c4b6a135807fd34b
Jannie87 <jannie.backman@gmail.com>
2023-01-03 09:20:19 +0100

    Added function for showing burgermenu in mobile and hide it in desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import brandName from './assets/text_logo.png';
 import './Header.css';
+import NavMobile from './Navbar';
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 790px)').matches
+  );
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 790px)')
+      .addEventListener('change', (e) => setIsMobile(e.matches));
+  }, []);
+
   return (
     <div className="header-container">
-      <nav className="navbar">
-        <img src={brandName} alt="brandName" />
-        <div className="nav-menu">
-          <ul>
-            <a href="#product-section">
-              <li>Tjänster</li>
-            </a>
-            <a href="#about-section">
-              <li>Om mig</li>
-            </a>
-            <a href="#contact-section">
-              <li>Kontakt</li>
-            </a>
-          </ul>
-        </div>
+      <nav>
+        {!isMobile ? (
+          <div className="navbar">
+            <img src={brandName} alt="brandName" />
+            <div className="nav-menu">
+              <ul>
+                <a href="#product-section">
+                  <li>Tjänster</li>
+                </a>
+                <a href="#about-section">
+                  <li>Om mig</li>
+                </a>
+                <a href="#contact-section">
+                  <li>Kontakt</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <NavMobile />
+        )}
       </nav>
     </div>
   );
