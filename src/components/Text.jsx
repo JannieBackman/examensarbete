4c3083d944a8fda02e477e263d967e521ffe0c0c
Jannie87 <jannie.backman@gmail.com>
2023-01-04 16:17:33 +0100

    now responsive - changed the typewritter function on smaller screens.

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,20 +1,54 @@
-import Typewriter from "typewriter-effect";
-import "./Text.css";
+import Typewriter from 'typewriter-effect';
+import './Text.css';
+import { useEffect, useState } from 'react';
 
 function Text() {
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 790px)').matches
+  );
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 790px)')
+      .addEventListener('change', (e) => setIsMobile(e.matches));
+  }, []);
   return (
-    <div className='textblock'>
-      <span className='writer'>
-        <Typewriter
-          options={{
-            strings: [
-              "Rörelseglädje - Träna tillsammans - Må bra - Funktionell träning ",
-            ],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-      </span>
+    <div className="textblock">
+      {!isMobile ? (
+        <span className="writer">
+          <Typewriter
+            options={{
+              strings: [
+                'Rörelseglädje - Träna tillsammans - Må bra - Funktionell träning ',
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </span>
+      ) : (
+        <span style={{ textAlign: 'center', padding: '1rem' }}>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('Rörelseglädje')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Träna tillsammans')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Må bra')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Funktionell träning')
+                .start();
+            }}
+          />
+        </span>
+      )}
     </div>
   );
 }
