e962334dcb31cf7c9dbcbbb27e7458ed7383d924
Jannie87 <jannie.backman@gmail.com>
2023-01-12 09:55:53 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,43 @@
-import heroImage from "./assets/heroImage.png";
-import boostLogo from "./assets/logo.png";
-import Header from "./Header";
+import heroImage from './assets/heroImage.png';
+import boostLogo from './assets/logo.png';
+import Header from './Header';
 
 function Hero() {
   return (
-    <div style={{ height: "100vh", width: "100%", position: " relative" }}>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        position: ' relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Header />
-      <img
-        src={heroImage}
-        alt=''
-        style={{ objectFit: "cover", height: "100%", width: "100%" }}
-      />
+      <img src={heroImage} alt="" style={{ height: '80%', width: '80%' }} />
+
       <div
         style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          position: 'absolute',
+          top: '40%',
+          left: '40%',
         }}
       >
-        <img src={boostLogo} alt='' style={{ width: "100%", height: "auto" }} />
+        <img src={boostLogo} alt="" style={{ width: '70%', height: 'auto' }} />
+      </div>
+      <div>
+        <h1
+          style={{
+            color: '#fff',
+            position: 'absolute',
+            top: '75%',
+            right: '25%',
+          }}
+        >
+          Utomhusträning i Kungsbacka
+        </h1>
       </div>
     </div>
   );
