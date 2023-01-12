563891ad54b26ba45e0a4b25ec64189ba8c81ba2
Jannie87 <jannie.backman@gmail.com>
2023-01-12 13:18:09 +0100

    collapsed text on screens below 790px

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,19 @@
 import aboutMe from './assets/AboutMe.png';
 import './About.css';
-import React, { useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
 
 function About() {
   const [open, setOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 790px)').matches
+  );
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 790px)')
+      .addEventListener('change', (e) => setIsMobile(e.matches));
+  }, []);
   return (
     <div className="about-container" id="about-section">
       <div className="about-imgTitle">
@@ -35,38 +43,57 @@ function About() {
           det!
         </p>
 
-        <div className="collapse-section">
-          <Button
-            style={{
-              backgroundColor: '#9eb89e',
-              border: 'none',
-              marginBottom: '1rem',
-            }}
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-          >
-            Läs mer
-          </Button>
-          <Collapse in={open}>
-            <div id="example-collapse-text">
-              <p>
-                Själv hittade jag Crossfit efter min första förlossning och
-                "resa tillbaka" och älskar fortfarande träningen lika mycket
-                idag, skillnaden idag är att jag har under resans gång lärt mig
-                otroligt mycket om kroppen och vikten utav att "träna rätt"
-                framförallt efter att jag slet av mitt korsband på en tävling
-                2021 som resulterande i operation och rehab.
-              </p>
-              <p>
-                Jag kör blandad träning så som löpning, crossfit och styrka och
-                min favvo övning är nog burpees. Träningen är även viktig för
-                mig i mitt yrke som kräver en hel del fysiskt. Men också för att
-                undvika skador och hålla kroppen frisk länge.
-              </p>
-            </div>
-          </Collapse>
-        </div>
+        {!isMobile ? (
+          <div>
+            <p>
+              Själv hittade jag Crossfit efter min första förlossning och "resa
+              tillbaka" och älskar fortfarande träningen lika mycket idag,
+              skillnaden idag är att jag har under resans gång lärt mig otroligt
+              mycket om kroppen och vikten utav att "träna rätt" framförallt
+              efter att jag slet av mitt korsband på en tävling 2021 som
+              resulterande i operation och rehab.
+            </p>
+            <p>
+              Jag kör blandad träning så som löpning, crossfit och styrka och
+              min favvo övning är nog burpees. Träningen är även viktig för mig
+              i mitt yrke som kräver en hel del fysiskt. Men också för att
+              undvika skador och hålla kroppen frisk länge.
+            </p>
+          </div>
+        ) : (
+          <div className="collapse-section">
+            <Button
+              style={{
+                backgroundColor: '#9eb89e',
+                border: 'none',
+                marginBottom: '1rem',
+              }}
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            >
+              Läs mer
+            </Button>
+            <Collapse in={open}>
+              <div id="example-collapse-text">
+                <p>
+                  Själv hittade jag Crossfit efter min första förlossning och
+                  "resa tillbaka" och älskar fortfarande träningen lika mycket
+                  idag, skillnaden idag är att jag har under resans gång lärt
+                  mig otroligt mycket om kroppen och vikten utav att "träna
+                  rätt" framförallt efter att jag slet av mitt korsband på en
+                  tävling 2021 som resulterande i operation och rehab.
+                </p>
+                <p>
+                  Jag kör blandad träning så som löpning, crossfit och styrka
+                  och min favvo övning är nog burpees. Träningen är även viktig
+                  för mig i mitt yrke som kräver en hel del fysiskt. Men också
+                  för att undvika skador och hålla kroppen frisk länge.
+                </p>
+              </div>
+            </Collapse>
+          </div>
+        )}
       </div>
     </div>
   );
