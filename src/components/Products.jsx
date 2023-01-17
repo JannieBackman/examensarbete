fd692e265b81d53aee2cec5696e289d923fd7fe3
Jannie87 <jannie.backman@gmail.com>
2023-01-17 13:10:34 +0100

    added  button and intro text

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,45 +1,65 @@
-import outdoorTraining from "./assets/outdoorTraining.png";
-import pt_online from "./assets/pt_online.png";
-import "./Products.css";
+import outdoorTraining from './assets/outdoorTraining.png';
+import pt_online from './assets/pt_online.png';
+import './Products.css';
+import Button from 'react-bootstrap/Button';
 
 function Products() {
   return (
-    <div className='products-container' id='product-section'>
+    <div className="products-container" id="product-section">
       <h1>Tjänster</h1>
-      <div className='outdoor'>
-        <p>
-          <strong>Utomhusträning</strong> <br />
-          Jag startade utomhusträningen hösten 2020 då det fanns utmaningar till
-          att vara inomhus på ett gym, sedan dess har vi kört på i alla väder
-          med träning ca 2 ggr/v året om. Träningen hos mig är varierande då ett
-          pass är aldrig det andra likt. Träningen sker med kroppsvikt,
-          gummiband, kettlebells, bollar, hantlar m.m. <br /> Det är viktigt för
-          mig att alla ska känna sig inkluderade och att jag hjälper just dig
-          till att anpassa träningen efter din nivå samt hjälper till ifall man
-          har skada eller liknande. <br /> <strong> ALLA </strong>kan vara med
-          och mår bra utav rörelse & att vi har väldigt kul tillsammans är ett
-          stort plus. <br /> Klicka här för se{" "}
-          <strong>Kommande träningspass</strong>
-        </p>
-        <img src={outdoorTraining} alt='outdoorworkout' />
-      </div>
-      <div className='pt'>
-        <p>
-          <strong>Personlig träning / Online träning </strong>
-          <br />
-          Personlig träning för dig som önskar få hjälp med att nå nya mål med
-          din träning och utveckla din fysiska förmåga eller för dig som vill
-          komma igång med träningen på ett hållbart och säkert sätt. Vi bygger
-          upp ett tränings-program som passar just dig, antingen på gym, hemma
-          eller ute eller online så du kan utföra träningen själv med hjälp utav
-          ett träningsprogram som du kan ha i en app i telefonen. Hör av dig
-          till mig på <a href='mailto: email@email.se'>Email</a> så hittar vi
-          ett upplägg som passar dig.
-        </p>
-        <img src={pt_online} alt='ptonline' />
+      <p className="intro">
+        Jag startade utomhusträningen hösten 2020 då det fanns utmaningar till
+        att vara inomhus på ett gym, sedan dess har vi kört på i alla väder med
+        träning ca 2 ggr/v året om. Träningen hos mig är varierande då ett pass
+        är aldrig det andra likt.
+      </p>
+
+      <div className="outdoor">
+        <div>
+          <p>
+            <strong>Utomhusträning</strong> <br />
+            Träningen sker med kroppsvikt, gummiband, kettlebells, bollar,
+            hantlar m.m. <br /> Det är viktigt för mig att alla ska känna sig
+            inkluderade och att jag hjälper just dig till att anpassa träningen
+            efter din nivå samt hjälper till ifall man har skada eller liknande.{' '}
+            <br /> <strong> ALLA </strong>kan vara med och mår bra utav rörelse
+            & att vi har väldigt kul tillsammans är ett stort plus. <br />{' '}
+            <Button
+              variant="dark"
+              href="https://fb.me/e/3RmOhc2SH"
+              style={{ marginTop: '1rem' }}
+            >
+              Kommande pass
+            </Button>
+          </p>
+        </div>
+
+        <div>
+          <img src={outdoorTraining} alt="outdoorworkout" />
+        </div>
       </div>
 
-      <div className='b2b'>
+      <div className="pt">
+        <div>
+          <p>
+            <strong>Personlig träning / Online träning </strong>
+            <br />
+            Personlig träning för dig som önskar få hjälp med att nå nya mål med
+            din träning och utveckla din fysiska förmåga eller för dig som vill
+            komma igång med träningen på ett hållbart och säkert sätt. Vi bygger
+            upp ett tränings-program som passar just dig, antingen på gym, hemma
+            eller ute eller online så du kan utföra träningen själv med hjälp
+            utav ett träningsprogram som du kan ha i en app i telefonen. Hör av
+            dig till mig på så hittar vi ett upplägg som passar dig.
+          </p>
+        </div>
+
+        <div>
+          <img src={pt_online} alt="ptonline" />
+        </div>
+      </div>
+
+      <div className="b2b">
         <p>
           <strong>Företagsträning </strong> <br />
           Är du sugen på att utmana & träna tillsammans med dina kollegor? Vill
@@ -48,7 +68,7 @@ function Products() {
           utomhus. Att utmanas tillsammans med sina arbetskamrater är väldogt
           roligt och utvecklade samt skapar en bättre sammanhållning och dynamik
           i gruppen. Vill du ha en offert? <br /> Maila mig på
-          <a href='mailto:'>Email</a>
+          <a href="mailto:">Email</a>
         </p>
       </div>
     </div>
