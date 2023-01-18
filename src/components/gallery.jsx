efdcb593a497ed6df471f9112829600c00d1f724
Jannie87 <jannie.backman@gmail.com>
2023-01-18 15:48:31 +0100

    fixed gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -4,21 +4,27 @@ import d from './assets/d.jpg';
 import e from './assets/e.jpg';
 import './gallery.css';
 import Form from './form';
+import { React, useEffect, useState } from 'react';
 
 function ImageGallery() {
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 790px)').matches
+  );
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 790px)')
+      .addEventListener('change', (e) => setIsMobile(e.matches));
+  }, []);
+
   return (
     <div className="container">
       <div className="form">
-        {' '}
         <Form />
       </div>
       <div className="gallery">
         <img src={b} alt="ptonline" width={250} />
         <img src={b} alt="ptonline" width={250} />
         <img src={c} alt="ptonline" width={250} />
-        <img src={d} alt="ptonline" width={250} />
-        <img src={e} alt="ptonline" width={250} />
-        <img src={e} alt="ptonline" width={250} />
       </div>
     </div>
   );
