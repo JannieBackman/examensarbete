5f9084d4e11780cb58ccd2cf9257448e3e02c44b
elinarnten <elin.arnten@hotmail.com>
2023-01-20 13:39:24 +0100

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import Hero from './Hero';
-import Text from './Text';
-import Products from './Products';
-import Review from './Review';
-import About from './About';
-import Footer from './Footer';
-import WaveBg from './Wavebg';
-import ImageGallery from './gallery';
-import B2b from './b2b';
+import Hero from "./Hero";
+import Text from "./Text";
+import Products from "./Products";
+import Review from "./Review";
+import About from "./About";
+import Footer from "./Footer";
+import WaveBg from "./Wavebg";
+import ImageGallery from "./gallery";
+import B2b from "./b2b";
 import Header from "./Header";
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
       <Hero />
       <Text />
       <Products />
-      <Review />
       <B2b />
       <WaveBg />
+      <Review />
       <About />
       <ImageGallery />
       <Footer />
