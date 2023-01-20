b4d5526df6b6e33cae5f4bc4072be985ee7f8c2e
Jannie87 <jannie.backman@gmail.com>
2023-01-20 10:57:26 +0100

    reorganized code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import About from './About';
 import Footer from './Footer';
 import WaveBg from './Wavebg';
 import ImageGallery from './gallery';
+import B2b from './b2b';
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <Hero />
       <Text />
       <Products />
+      <Review />
+      <B2b />
       <WaveBg />
       <About />
-      <Review />
       <ImageGallery />
       <Footer />
     </div>
