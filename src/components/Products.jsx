2c08c92d564a877ec9cefe9e6e76c80588331d5a
Jannie87 <jannie.backman@gmail.com>
2023-01-24 13:56:49 +0100

    new design to products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,62 +1,65 @@
-import outdoorTraining from "./assets/39.png";
-import pt_online from "./assets/40.png";
-import "./Products.css";
-import Button from "react-bootstrap/Button";
+import workout from '../components/assets/DSC05312.JPG';
+import workout2 from '../components/assets/DSC05584.JPG';
+
+import './Products.css';
 
 function Products() {
   return (
-    <div className='products-container' id='product-section'>
-      <h1>Tjänster</h1>
-      <p className='intro'>
-        Jag startade utomhusträningen hösten 2020 då det fanns utmaningar till
-        att vara inomhus på ett gym, sedan dess har vi kört på i alla väder med
-        träning ca 2 ggr/v året om. Träningen hos mig är varierande då ett pass
-        är aldrig det andra likt.
-      </p>
-
-      <div className='outdoor'>
-        <div className='outdoorText'>
-          <h3>Utomhusträning</h3> <br />
-          <p>
-            Träningen sker med kroppsvikt, gummiband, kettlebells, bollar,
-            hantlar m.m. <br /> Det är viktigt för mig att alla ska känna sig
-            inkluderade och att jag hjälper just dig till att anpassa träningen
-            efter din nivå samt hjälper till ifall man har skada eller liknande.{" "}
-            <br /> <strong> ALLA </strong>kan vara med och mår bra utav rörelse
-            & att vi har väldigt kul tillsammans är ett stort plus. <br />{" "}
-            <Button
-              variant='dark'
-              href='https://fb.me/e/3RmOhc2SH'
-              style={{ marginTop: "1rem" }}
-            >
-              Kommande pass
-            </Button>
-          </p>
-        </div>
-
-        <div className='outdoorImg'>
-          <img src={outdoorTraining} alt='outdoorworkout' />
-        </div>
+    <div className="products-container" id="product-section">
+      <h1> Tjänster</h1>
+      <div
+        style={{
+          display: 'flex',
+          margin: '5rem',
+          border: 'solid #778a77 5px',
+        }}
+      >
+        <p
+          style={{
+            fontSize: '1.5rem',
+            padding: '4rem',
+            textAlign: 'center',
+          }}
+        >
+          <h2> Utomhusträning</h2>
+          Jag startade utomhusträningen hösten 2020 då det fanns utmaningar till
+          att vara inomhus på ett gym, sedan dess har vi kört på i alla väder
+          med träning ca 2 ggr/v året om. Träningen hos mig är varierande då ett
+          pass är aldrig det andra likt. Träningen sker med kroppsvikt,
+          gummiband, kettlebells, bollar, hantlar m.m. <br /> Det är viktigt för
+          mig att alla ska känna sig inkluderade och att jag hjälper just dig
+          till att anpassa träningen efter din nivå samt hjälper till ifall man
+          har skada eller liknande. <br /> <strong> ALLA </strong>kan vara med
+          och mår bra utav rörelse & att vi har väldigt kul tillsammans är ett
+          stort plus.
+        </p>
+        <img src={workout} alt="workout" height={600} />
       </div>
-
-      <div className='pt'>
-        <div className='ptText'>
-          <h3>Personlig träning / Online träning </h3>
-          <p>
-            <br />
-            Personlig träning för dig som önskar få hjälp med att nå nya mål med
-            din träning och utveckla din fysiska förmåga eller för dig som vill
-            komma igång med träningen på ett hållbart och säkert sätt. Vi bygger
-            upp ett tränings-program som passar just dig, antingen på gym, hemma
-            eller ute eller online så du kan utföra träningen själv med hjälp
-            utav ett träningsprogram som du kan ha i en app i telefonen. Hör av
-            dig till mig på så hittar vi ett upplägg som passar dig.
-          </p>
-        </div>
-
-        <div className='ptImg'>
-          <img src={pt_online} alt='ptonline' />
-        </div>
+      <div
+        style={{
+          display: 'flex',
+          margin: '5rem',
+          border: 'solid #778a77 5px',
+          flexDirection: 'row-reverse',
+        }}
+      >
+        <p
+          style={{
+            fontSize: '1.5rem',
+            padding: '4rem',
+            textAlign: 'center',
+          }}
+        >
+          <h2> Personlig träning</h2>
+          Personlig träning för dig som önskar få hjälp med att nå nya mål med
+          din träning och utveckla din fysiska förmåga eller för dig som vill
+          komma igång med träningen på ett hållbart och säkert sätt. Vi bygger
+          upp ett tränings-program som passar just dig, antingen på gym, hemma
+          eller ute eller online så du kan utföra träningen själv med hjälp utav
+          ett träningsprogram som du kan ha i en app i telefonen. Hör av dig
+          till mig på så hittar vi ett upplägg som passar dig.
+        </p>
+        <img src={workout2} alt="workout" height={600} />
       </div>
     </div>
   );
