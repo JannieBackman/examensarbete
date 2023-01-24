dadd706c0cef90ec9889ae2c47143647adf3e155
Jannie87 <jannie.backman@gmail.com>
2023-01-24 10:23:47 +0100

    new hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,57 +1,78 @@
-import heroImage from "./assets/heroImage2.png";
-import boostLogo from "./assets/logo.png";
+import heroImage from './assets/hero_bw.jpg';
+import boostLogo from './assets/logo.png';
 
 function Hero() {
   return (
     <div
       style={{
-        position: " relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
+        position: ' relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 600,
       }}
     >
       <img
         src={heroImage}
-        alt='outdoorworkout'
-        style={{ height: "100%", width: "100%" }}
+        alt="outdoorworkout"
+        style={{ height: '100%', width: '100%' }}
       />
 
       <div
         style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
         }}
       >
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justtifyContent: "center",
-            alignItems: "center",
-            width: "100%",
+            display: 'flex',
+            flexDirection: 'column',
+            justtifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
           }}
         >
           <img
             src={boostLogo}
-            alt='logotype'
-            style={{ width: "40%", height: "40%" }}
+            alt="logotype"
+            style={{ width: '40%', height: '40%' }}
           />
+          <div style={{ textAlign: 'center' }}>
+            <h1
+              style={{
+                color: '#778A77',
+                fontFamily: 'Righteous',
+                fontWeight: '600',
+                fontSize: '4rem',
+                letterSpacing: '2rem',
+              }}
+            >
+              Josefin
+            </h1>
+            <h1
+              style={{
+                color: '#fff',
+                fontFamily: 'Righteous',
+                fontWeight: '600',
+                fontSize: '4rem',
+                letterSpacing: '2rem',
+              }}
+            >
+              Lundqvist
+            </h1>
 
-          <h1
-            style={{
-              color: "#fff",
-              display: "flex",
-              textAlign: "center",
-              width: "100%",
-              height: "auto",
-            }}
-          >
-            Utomhusträning i Kungsbacka
-          </h1>
+            <span
+              style={{
+                color: '#fff',
+              }}
+            >
+              Fitness Coach
+            </span>
+          </div>
         </div>
       </div>
     </div>
