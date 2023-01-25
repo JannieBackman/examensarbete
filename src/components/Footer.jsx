e0d8c305ced33145a8914735e2e48e5dd9a2b3e9
elinarnten <elin.arnten@hotmail.com>
2023-01-25 16:07:49 +0100

    fix footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,35 @@
-import './Footer.css';
-import dumbell1 from './assets/dumbell1.jpg';
-import Form from './form';
-import Socials from './socials';
+import "./Footer.css";
+import dumbell1 from "./assets/dumbell1.jpg";
+import Form from "./form";
+import Socials from "./socials";
 
 function Footer() {
   return (
-    <div className="footer-container" id="contact-section">
-      <div style={{ display: 'flex' }}>
+    <div className='footer-container' id='contact-section'>
+      <div style={{ display: "flex" }}>
         <div
           style={{
-            width: '50%',
-            position: 'relative',
+            width: "50%",
+            position: "relative",
           }}
         >
           <img
             src={dumbell1}
-            alt="dumbell"
+            alt='dumbell'
             style={{
-              width: '100%',
-              objectFit: 'fill',
+              width: "100%",
+              height: "100%",
+              objectFit: "fill",
             }}
           />
 
           <Socials />
         </div>
-        <div style={{ background: '#778a77', width: '50%' }}>
+        <div style={{ background: "#778a77", width: "50%" }}>
           <Form />
         </div>
       </div>
-      <div className="copyright">
+      <div className='copyright'>
         <span>Copyright © 2023 boostbyjossan</span>
       </div>
     </div>
