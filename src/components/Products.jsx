75357b31274f615a59308efe4bc8e1d18daa9c43
Jannie87 <jannie.backman@gmail.com>
2023-01-25 12:21:41 +0100

    products responsive

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,96 +19,101 @@ function Products() {
     <div className="products-container" id="product-section">
       <h1> Tjänster</h1>
       <div className="outdoor-content">
-        <h2> Utomhusträning</h2>
-        <p className="outdoor-text">
-          Jag startade utomhusträningen hösten 2020 då det fanns utmaningar till
-          att vara inomhus på ett gym, sedan dess har vi kört på i alla väder
-          med träning ca 2 ggr/v året om.
-        </p>
-        {!isMobile ? (
+        <div className="outdoor-text">
+          <h2> Utomhusträning</h2>
           <p>
-            Träningen hos mig är varierande då ett pass är aldrig det andra
-            likt. Träningen sker med kroppsvikt, gummiband, kettlebells, bollar,
-            hantlar m.m. <br /> Det är viktigt för mig att alla ska känna sig
-            inkluderade och att jag hjälper just dig till att anpassa träningen
-            efter din nivå samt hjälper till ifall man har skada eller liknande.{' '}
-            <br /> <strong> ALLA </strong>kan vara med och mår bra utav rörelse
-            & att vi har väldigt kul tillsammans är ett stort plus.
+            Jag startade utomhusträningen hösten 2020 då det fanns utmaningar
+            till att vara inomhus på ett gym, sedan dess har vi kört på i alla
+            väder med träning ca 2 ggr/v året om.
           </p>
-        ) : (
-          <div className="collapse-section">
-            <Button
-              style={{
-                backgroundColor: '#9eb89e',
-                border: 'none',
-                marginBottom: '1rem',
-              }}
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              Läs mer
-            </Button>
-            <Collapse in={open}>
-              <div id="example-collapse-text">
-                <p>
-                  Träningen hos mig är varierande då ett pass är aldrig det
-                  andra likt. Träningen sker med kroppsvikt, gummiband,
-                  kettlebells, bollar, hantlar m.m. <br /> Det är viktigt för
-                  mig att alla ska känna sig inkluderade och att jag hjälper
-                  just dig till att anpassa träningen efter din nivå samt
-                  hjälper till ifall man har skada eller liknande. <br />{' '}
-                  <strong> ALLA </strong>kan vara med och mår bra utav rörelse &
-                  att vi har väldigt kul tillsammans är ett stort plus.
-                </p>
-              </div>
-            </Collapse>
-          </div>
-        )}
+          {!isMobile ? (
+            <p>
+              Träningen hos mig är varierande då ett pass är aldrig det andra
+              likt. Träningen sker med kroppsvikt, gummiband, kettlebells,
+              bollar, hantlar m.m. <br /> Det är viktigt för mig att alla ska
+              känna sig inkluderade och att jag hjälper just dig till att
+              anpassa träningen efter din nivå samt hjälper till ifall man har
+              skada eller liknande. <br /> <strong> ALLA </strong>kan vara med
+              och mår bra utav rörelse & att vi har väldigt kul tillsammans är
+              ett stort plus.
+            </p>
+          ) : (
+            <div className="collapse-section">
+              <Button
+                style={{
+                  backgroundColor: '#9eb89e',
+                  border: 'none',
+                  marginBottom: '1rem',
+                }}
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+              >
+                Läs mer
+              </Button>
+              <Collapse in={open}>
+                <div id="example-collapse-text">
+                  <p className="outdoor-text">
+                    Träningen hos mig är varierande då ett pass är aldrig det
+                    andra likt. Träningen sker med kroppsvikt, gummiband,
+                    kettlebells, bollar, hantlar m.m. <br /> Det är viktigt för
+                    mig att alla ska känna sig inkluderade och att jag hjälper
+                    just dig till att anpassa träningen efter din nivå samt
+                    hjälper till ifall man har skada eller liknande. <br />{' '}
+                    <strong> ALLA </strong>kan vara med och mår bra utav rörelse
+                    & att vi har väldigt kul tillsammans är ett stort plus.
+                  </p>
+                </div>
+              </Collapse>
+            </div>
+          )}
+        </div>
         <img src={workout} alt="workout" height={600} />
       </div>
       <div className="pt-content">
-        <h2> Personlig träning</h2>
-        <p className="pt-text">
-          Personlig träning för dig som önskar få hjälp med att nå nya mål med
-          din träning och utveckla din fysiska förmåga eller för dig som vill
-          komma igång med träningen på ett hållbart och säkert sätt.
-        </p>
-        {!isMobile ? (
+        <div className="pt-text">
+          <h2> Personlig träning</h2>
           <p>
-            Vi bygger upp ett tränings-program som passar just dig, antingen på
-            gym, hemma eller ute eller online så du kan utföra träningen själv
-            med hjälp utav ett träningsprogram som du kan ha i en app i
-            telefonen. Hör av dig till mig på så hittar vi ett upplägg som
-            passar dig.
+            Personlig träning för dig som önskar få hjälp med att nå nya mål med
+            din träning och utveckla din fysiska förmåga eller för dig som vill
+            komma igång med träningen på ett hållbart och säkert sätt.
           </p>
-        ) : (
-          <div className="collapse-section">
-            <Button
-              style={{
-                backgroundColor: '#9eb89e',
-                border: 'none',
-                marginBottom: '1rem',
-              }}
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              Läs mer
-            </Button>
-            <Collapse in={open}>
-              <div id="example-collapse-text">
-                <p>
-                  Vi bygger upp ett tränings-program som passar just dig,
-                  antingen på gym, hemma eller ute eller online så du kan utföra
-                  träningen själv med hjälp utav ett träningsprogram som du kan
-                  ha i en app i telefonen. Hör av dig till mig på så hittar vi
-                  ett upplägg som passar dig.
-                </p>
-              </div>
-            </Collapse>
-          </div>
-        )}
+          {!isMobile ? (
+            <p>
+              Vi bygger upp ett tränings-program som passar just dig, antingen
+              på gym, hemma eller ute eller online så du kan utföra träningen
+              själv med hjälp utav ett träningsprogram som du kan ha i en app i
+              telefonen. Hör av dig till mig på så hittar vi ett upplägg som
+              passar dig.
+            </p>
+          ) : (
+            <div className="collapse-section">
+              <Button
+                style={{
+                  backgroundColor: '#9eb89e',
+                  border: 'none',
+                  marginBottom: '1rem',
+                }}
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+              >
+                Läs mer
+              </Button>
+              <Collapse in={open}>
+                <div id="example-collapse-text">
+                  <p>
+                    Vi bygger upp ett tränings-program som passar just dig,
+                    antingen på gym, hemma eller ute eller online så du kan
+                    utföra träningen själv med hjälp utav ett träningsprogram
+                    som du kan ha i en app i telefonen. Hör av dig till mig på
+                    så hittar vi ett upplägg som passar dig.
+                  </p>
+                </div>
+              </Collapse>
+            </div>
+          )}
+        </div>
         <img src={workout2} alt="workout" height={600} />
       </div>
       <div className="quote-content">
