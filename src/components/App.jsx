ecc2a3a374e24536d5a0904bff58b2688dc29506
Jannie87 <jannie.backman@gmail.com>
2023-01-26 13:55:01 +0100

    added gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import Hero from './Hero';
 import Text from './Text';
-import Products from './Products';
+import { ProductBottom, ProductHeader, ProductTop } from './Products';
 import Review from './Review';
 import About from './About';
 import Footer from './Footer';
 import B2b from './b2b';
 import Header from './Header';
+import ImageGallery from './gallery';
 
 function App() {
   return (
@@ -13,10 +14,13 @@ function App() {
       <Header />
       <Hero />
       <Text />
-      <Products />
+      <ProductHeader />
+      <ProductTop />
+      <ProductBottom />
       <Review />
       <B2b />
       <About />
+      <ImageGallery />
       <Footer />
     </div>
   );
