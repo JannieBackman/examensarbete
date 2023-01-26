0b9253699effd9ef4dbc2cf0b2d1b89332ec2f5e
elinarnten <elin.arnten@hotmail.com>
2023-01-26 14:15:54 +0100

    responsive and design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-import Hero from './Hero';
-import Text from './Text';
-import Products from './Products';
-import Review from './Review';
-import About from './About';
-import Footer from './Footer';
-import B2b from './b2b';
-import Header from './Header';
+import Hero from "./Hero";
+import Text from "./Text";
+import Products from "./Products";
+import About from "./About";
+import Footer from "./Footer";
+import B2b from "./b2b";
+import Header from "./Header";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <Hero />
       <Text />
       <Products />
-      <Review />
       <B2b />
       <About />
       <Footer />
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,25 +1,27 @@
-import workout from '../components/assets/DSC05312.JPG';
-import workout2 from '../components/assets/DSC05584.JPG';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
-import './Products.css';
-import { React, useEffect, useState } from 'react';
+import workout from "../components/assets/DSC05312.JPG";
+import workout2 from "../components/assets/DSC05584.JPG";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import "./Products.css";
+import { React, useEffect, useState } from "react";
+import Review from "./Review";
 
 function Products() {
   const [open, setOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(
-    window.matchMedia('(max-width: 790px)').matches
+    window.matchMedia("(max-width: 790px)").matches
   );
   useEffect(() => {
     window
-      .matchMedia('(max-width: 790px)')
-      .addEventListener('change', (e) => setIsMobile(e.matches));
+      .matchMedia("(max-width: 790px)")
+      .addEventListener("change", (e) => setIsMobile(e.matches));
   }, []);
   return (
-    <div className="products-container" id="product-section">
+    <div className='products-container' id='product-section'>
       <h1> Tjänster</h1>
-      <div className="outdoor-content">
-        <div className="outdoor-text">
+
+      <div className='outdoor-content'>
+        <div className='outdoor-text'>
           <h2> Utomhusträning</h2>
           <p>
             Jag startade utomhusträningen hösten 2020 då det fanns utmaningar
@@ -38,28 +40,28 @@ function Products() {
               ett stort plus.
             </p>
           ) : (
-            <div className="collapse-section">
+            <div className='collapse-section'>
               <Button
                 style={{
-                  backgroundColor: '#9eb89e',
-                  border: 'none',
-                  marginBottom: '1rem',
+                  backgroundColor: "#9eb89e",
+                  border: "none",
+                  marginBottom: "1rem",
                 }}
                 onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
+                aria-controls='example-collapse-text'
                 aria-expanded={open}
               >
                 Läs mer
               </Button>
               <Collapse in={open}>
-                <div id="example-collapse-text">
-                  <p className="outdoor-text">
+                <div id='example-collapse-text'>
+                  <p className='outdoor-text'>
                     Träningen hos mig är varierande då ett pass är aldrig det
                     andra likt. Träningen sker med kroppsvikt, gummiband,
                     kettlebells, bollar, hantlar m.m. <br /> Det är viktigt för
                     mig att alla ska känna sig inkluderade och att jag hjälper
                     just dig till att anpassa träningen efter din nivå samt
-                    hjälper till ifall man har skada eller liknande. <br />{' '}
+                    hjälper till ifall man har skada eller liknande. <br />{" "}
                     <strong> ALLA </strong>kan vara med och mår bra utav rörelse
                     & att vi har väldigt kul tillsammans är ett stort plus.
                   </p>
@@ -68,10 +70,12 @@ function Products() {
             </div>
           )}
         </div>
-        <img src={workout} alt="workout" height={600} />
+
+        <img src={workout} alt='workout' height={600} />
       </div>
-      <div className="pt-content">
-        <div className="pt-text">
+      <Review />
+      <div className='pt-content'>
+        <div className='pt-text'>
           <h2> Personlig träning</h2>
           <p>
             Personlig träning för dig som önskar få hjälp med att nå nya mål med
@@ -87,21 +91,21 @@ function Products() {
               passar dig.
             </p>
           ) : (
-            <div className="collapse-section">
+            <div className='collapse-section'>
               <Button
                 style={{
-                  backgroundColor: '#9eb89e',
-                  border: 'none',
-                  marginBottom: '1rem',
+                  backgroundColor: "#9eb89e",
+                  border: "none",
+                  marginBottom: "1rem",
                 }}
                 onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
+                aria-controls='example-collapse-text'
                 aria-expanded={open}
               >
                 Läs mer
               </Button>
               <Collapse in={open}>
-                <div id="example-collapse-text">
+                <div id='example-collapse-text'>
                   <p>
                     Vi bygger upp ett tränings-program som passar just dig,
                     antingen på gym, hemma eller ute eller online så du kan
@@ -114,16 +118,17 @@ function Products() {
             </div>
           )}
         </div>
-        <img src={workout2} alt="workout" height={600} />
+        <img src={workout2} alt='workout' height={600} />
       </div>
-      <div className="quote-content">
-        <p className="quote-text">
+      <div className='quote-content'>
+        <p className='quote-text'>
           <strong>
-            <span class="material-symbols-outlined">format_quote</span>
+            <span class='material-symbols-outlined'>format_quote</span>
             Jag brinner för att peppa, inspirera och älskar att se hur individen
             utvecklas och vågar ta sig utanför sin comfortzone och framförallt
-            hjälpa till att skapa starka & hållbara kroppar.{' '}
-            <span class="material-symbols-outlined">format_quote</span>
+            hjälpa till att skapa starka & hållbara kroppar.{" "}
+            <span class='material-symbols-outlined'>format_quote</span>
+            <p>-Josefin</p>
           </strong>
         </p>
       </div>
